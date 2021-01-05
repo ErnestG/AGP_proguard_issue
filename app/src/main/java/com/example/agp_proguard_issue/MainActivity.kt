83682645f46a23166bcb416d99db20d03700e04b
Ernest Grzybowski <me@ernestg.com>
2021-01-05 17:32:03 -0500

    add firebase analytics

--- app/src/main/java/com/example/agp_proguard_issue/MainActivity.kt
+++ app/src/main/java/com/example/agp_proguard_issue/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.agp_proguard_issue
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        FirebaseAnalytics.getInstance(this)
     }
 }
